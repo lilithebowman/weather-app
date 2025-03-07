995a320c71b600254ef5b22a28926ab1450c8f26
Lilithe Bowman <lilithe.bowman@gmail.com>
2025-03-07 09:30:50 -0500

    the beginnings of some display logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,17 @@ import { Formik, Field, Form } from 'formik'
 function App() {
 	const [city, setCity] = useState('Toronto')
 	const { data: weather, isError: isError } = useWeatherDetails(city)
+	console.log(weather)
 
 	return isError ? (<div>Error</div>) : (
 		<>
 			<div>
 				<h1>Weather</h1>
-				<div>
-					{JSON.stringify(weather)}
-				</div>
+				<div className="weatherCity">{weather?.location.name}, {weather?.location.region}</div>
+				<div className="weatherTemp">{weather?.current.temp_c}°C / {weather?.current.temp_f}°F</div>
+				<div className="weatherCondition">{weather?.current.condition.text}</div>
+				<div className="weatherWind">Wind: {weather?.current.wind_kph} km/h</div>
+				<div className="weatherHumidity">Humidity: {weather?.current.humidity}%</div>
 
 				<Formik
 					initialValues={{ city: '' }}
