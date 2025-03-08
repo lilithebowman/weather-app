f6bb8190078572e3c94a7d4e9552fdd3ec7db134
Lilithe Bowman <lilithe.bowman@gmail.com>
2025-03-08 16:39:11 -0500

    TextEncoder missing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,8 @@
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as any;
+
 jest.mock('./config/config.ts', () => ({
-	VITE_WEATHER_API_KEY: 'w4X8C1ff4tlrhTPOP0Lo8ba4CgsOMMP3tlZiiMzRjj4'
+	VITE_WEATHER_API_KEY: 'TEST_API_KEY'
 }));
